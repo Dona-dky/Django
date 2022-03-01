aa16d75c646d4f69b52484abf8d05436ed85b3bd
Dona <dona.denkey_dandjein@edu.devinci.fr>
2022-03-01 08:27:21 +0100

    add view to create a post

--- theapp/views.py
+++ theapp/views.py
@@ -3,8 +3,21 @@ from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.http import HttpResponse, response, Http404, HttpResponseRedirect
 
 from .models import BlogPost
-from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 def index():
     return render('theapp/index.html')
+
+def new_post(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+
+    # add the dictionary during initialization
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    
+    context['form']= form
+    return render(request, 'theapp/new_post.html', context)

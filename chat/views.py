3de94acab4f101e55772e3da92b684264d7c0185
Dona <dona.denkey_dandjein@edu.devinci.fr>
2022-03-01 20:40:35 +0100

    init view to chat

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from chat.models import Room
+
+#
+def home_view(request):
+    return render(request, 'home.html', {
+        'rooms': Room.objects.all(),
+    })
+
+#
+def room_view(request, room_name):
+    chat_room, created = Room.objects.get_or_create(name=room_name)
+    return render(request, 'room.html', {
+        'room': chat_room,
+    })

ade15f6eb08e0cd7e002a05b213fea2ce61a6a3f
Dona <dona.denkey_dandjein@edu.devinci.fr>
2022-03-01 20:38:23 +0100

    update admin panel

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from chat.models import Room, Message
 
 # Register your models here.
+
+# Models will be accessible in Admin panel
+admin.site.register(Room)
+admin.site.register(Message)

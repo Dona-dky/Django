c98d7f7c89b181b094746907a8aee94cc1a22d14
Dona <dona.denkey_dandjein@edu.devinci.fr>
2022-03-01 20:42:50 +0100

    access to page

--- chat/views.py
+++ chat/views.py
@@ -6,13 +6,13 @@ from chat.models import Room
 
 #
 def home_view(request):
-    return render(request, 'home.html', {
+    return render(request, 'chat/home.html', {
         'rooms': Room.objects.all(),
     })
 
 #
 def room_view(request, room_name):
     chat_room, created = Room.objects.get_or_create(name=room_name)
-    return render(request, 'room.html', {
+    return render(request, 'chat/room.html', {
         'room': chat_room,
     })

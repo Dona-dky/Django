01aba3cc9d0680c362bc91b7a6100eb5de01481f
Dona <dona.denkey_dandjein@edu.devinci.fr>
2022-03-01 20:35:25 +0100

    init models chat

--- django_eval/settings.py
+++ django_eval/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'theapp.apps.TheappConfig',
     'beautyflowers.apps.BeautyflowersConfig',
+    'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_eval/urls.py
+++ django_eval/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('theapp/', include('theapp.urls')),
     path('admin/', admin.site.urls),
     path('', include('beautyflowers.urls')),
+    path('chat/', include('chat.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
